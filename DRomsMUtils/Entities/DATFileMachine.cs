a01f00b1d6fc2fb9a3cbeacc5f83cc9f84bc8b2f
Dreas <agnt666@gmail.com>
2022-05-05 02:40:45 +0200

    Changing some properties to fields for performance reasons

--- DRomsMUtils/Entities/DATFileMachine.cs
+++ DRomsMUtils/Entities/DATFileMachine.cs
@@ -6,37 +6,37 @@ public enum DATFileMachineScreenOrientation
     Vertical
 }
 
+/// <summary>
+/// Fields are used in this class instead of properties for performance reasons
+/// </summary>
 public class DATFileMachine
 {
     [CsvHelper.Configuration.Attributes.Name("Set")]
-    public string Name { get; set; }
+    public string Name; // { get; set; }
 
     [CsvHelper.Configuration.Attributes.Name("Name")]
-    public string Description { get; set; }
+    public string Description; // { get; set; }
 
-    public string Year { get; set; }
-    public string Manufacturer { get; set; }
-    public string Status { get; set; }
-    public string Emulation { get; set; }
-    public string SaveStates { get; set; }
-    public string Players { get; set; }
-    public string Coins { get; set; }
-    public string Controls { get; set; }
-    public string ScreenType { get; set; }
-    public DATFileMachineScreenOrientation ScreenOrientation { get; set; }
-    public string ScreenRefreshRate { get; set; }
-    public string IsBIOS { get; set; }
-    public string IsClone { get; set; }
-    public string IsMechanical { get; set; }
-    public string RequireCHDs { get; set; }
-    public string RequireSamples { get; set; } 
-    public string IsDevice { get; set; }
+    public string Year; // { get; set; }
+    public string Manufacturer; // { get; set; }
+    public string Status; // { get; set; }
+    public string Emulation; // { get; set; }
+    public string SaveStates; // { get; set; }
+    public string Players; // { get; set; }
+    public string Coins; // { get; set; }
+    public string Controls; // { get; set; }
+    public string ScreenType; // { get; set; }
+    public DATFileMachineScreenOrientation ScreenOrientation; // { get; set; }
+    public string ScreenRefreshRate; // { get; set; }
+    public string IsBIOS; // { get; set; }
+    public string IsClone; // { get; set; }
+    public string IsMechanical; // { get; set; }
+    public string RequireCHDs; // { get; set; }
+    public string RequireSamples; // { get; set; } 
+    public string IsDevice; // { get; set; }
 
-    // public string ParentRom { get; set; }
-
-    public int MAMESortingIndex { get; set; }
-    // public bool IsDevice { get; set; }
-    public string XMLValue { get; set; }
+    public int MAMESortingIndex; // { get; set; }
+    public string XMLValue; // { get; set; }
 
     public override string ToString()
     {
