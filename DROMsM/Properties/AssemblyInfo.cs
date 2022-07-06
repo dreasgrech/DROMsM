a7be1b70e8f2a027a14bc2c0f50302872ff32a2e
Dreas <agnt666@gmail.com>
2022-07-06 13:51:57 +0200

    bump to v1.0.18

--- DROMsM/Properties/AssemblyInfo.cs
+++ DROMsM/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.0.*")]
 //[assembly: AssemblyVersion("1.0.0.2")]
 //[assembly: AssemblyFileVersion("1.0.0.2")]
-[assembly: AssemblyVersion("1.0.17")]
-[assembly: AssemblyFileVersion("1.0.17")]
+[assembly: AssemblyVersion("1.0.18")]
+[assembly: AssemblyFileVersion("1.0.18")]
