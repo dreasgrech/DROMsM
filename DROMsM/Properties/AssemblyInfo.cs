aae4560c5fa5e80bc0e79f877ee3815835d74a03
Andreas Grech <agnt666@gmail.com>
2024-12-31 21:06:44 +0100

    Bump version from 1.0.22 to 1.0.23

--- DROMsM/Properties/AssemblyInfo.cs
+++ DROMsM/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dreas")]
 [assembly: AssemblyProduct("DROMsM")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.0.*")]
 //[assembly: AssemblyVersion("1.0.0.2")]
 //[assembly: AssemblyFileVersion("1.0.0.2")]
-[assembly: AssemblyVersion("1.0.22")]
-[assembly: AssemblyFileVersion("1.0.22")]
+[assembly: AssemblyVersion("1.0.23")]
+[assembly: AssemblyFileVersion("1.0.23")]
